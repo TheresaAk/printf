e5bea2444199b14048ba77ee2d3ec67b95e8cc29
TheresaAk <akinbolatheresa19@email.com>
2023-03-25 16:53:49 -0700

    _printf.c

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+void _putchar(char c);
+void _puts(char *str);
+char *_itoa(int number);
+char *_utoa(unsigned int number, int base, int uppercase);
+unsigned int _strlen(char *s);
+int _printf(const char *format, ...);
+#endif
